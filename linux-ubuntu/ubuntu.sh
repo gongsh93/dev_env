17ec2e0a0397a10092d06102d1ae8d11f62c880f
gongsh93@gmail.com <gongsh93@gmail.com>
2020-02-28 21:50:32 +0900

    ubuntu files tree view

--- linux-ubuntu/ubuntu.sh
+++ linux-ubuntu/ubuntu.sh
@@ -1,13 +1,16 @@
 echo "Installing essential tools for ubuntu environment..."
 sudo apt-get install vim -y
+echo "Essential tools installation done."
+
+echo "Google Chrome Installation"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
-echo "Essential tools installation done."
 
 
 echo "Installing GUI tools for ubuntu..."
 sudo apt-get install gnome-tweaks -y
+sudo apt-get install dconf-editor -y
 gsettings set org.gnome.desktop.interface gtk-theme "HighContrastInverse"
 gsettings set org.gnome.desktop.interface cursor-theme "redglass"
 gsettings set org.gnome.desktop.interface cursor-size 50
@@ -22,5 +25,6 @@ gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profi
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ use-theme-transparency false
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ use-transparent-background true
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ background-transparency-percent 10
+gsettings set org.gnome.nautilus.list-view use-tree-view true
 echo "GUI tools installationg done."
 
